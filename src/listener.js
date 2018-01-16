d1f2cf1dd5e77eb9be66b946a392d233c79fdbe2
dok <dumansky@gmail.com>
2018-01-16 17:58:50 +0200

    listener

---
+++ src/listener.js
@@ -0,0 +1,46 @@
+import {UPDATE} from "./utils/events.js";
+
+let tacoData = require('./tacodata.js');
+
+let window = window || global.window;
+
+function update(data){
+    for(var template in tacoData.main) {
+        var key = getKey(data, template);
+        for(var control in tacoData.main[template]){
+            if(data[key] && data[key].hasOwnProperty(control)){
+                tacoData.main[template][control] = data[key][control];
+            }
+        }
+
+    }
+
+    if(tacoData.updateCB){
+        tacoData.updateCB();
+    }
+}
+
+function getKey(data, keyToFind) {
+    var keys = Object.keys(data);
+    for(var i = 0 ; i < keys.length ; i++){
+        if(keys[i].toLowerCase() === keyToFind.toLowerCase()){
+            return keys[i];
+        }
+    }
+}
+
+var handlers = {};
+handlers[UPDATE] = update;
+
+
+function messageHandler(message){
+    var messageData = JSON.parse(message.data);
+    var type = messageData.type;
+    var handler = handlers[type];
+    if(handler){
+        handler(messageData.payload);
+    }
+}
+if(window && window.addEventListener){
+    window.addEventListener('message', messageHandler);
+}
