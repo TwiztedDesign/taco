5b1f94072223c5d4b21f5ade81a19eff811518df
Guy Brill <brill.guy1@gmail.com>
2018-01-28 13:23:15 +0200

    added sinon for spying on object methods test for go methond on api

--- test/core/api.test.js
+++ test/core/api.test.js
@@ -1,13 +1,15 @@
-const expect = require('chai').expect;
-const should = require('chai').should;
-const api = require('../../src/core/api.js');
+const sinon     = require('sinon');
+const assert    = require('chai').assert;
+const expect    = require('chai').expect;
+const api       = require('../../src/core/api.js');
 const messenger = require('../../src/utils/messenger.js');
 
 describe("api", () =>{
-   describe.skip("Go", () => {
+   describe("Go", () => {
       it("Should post a massage", () => {
+          let send = sinon.spy(messenger, 'send');
           api.go('test', 0);
-          messenger.send.should.have.been.called;
+          assert(send.called);
       });
    });
 });
