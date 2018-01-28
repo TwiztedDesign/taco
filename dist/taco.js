e55d95e5593ee1b38fb12745b1328dc4c51998ba
Guy Brill <brill.guy1@gmail.com>
2018-01-28 17:50:10 +0200

    taco data class

--- dist/taco.js
+++ dist/taco.js
@@ -317,6 +317,8 @@ var _messenger = __webpack_require__(1);
 
 var _events = __webpack_require__(0);
 
+var _tacodata = __webpack_require__(2);
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -324,7 +326,6 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 __webpack_require__(6).start();
-var tacoData = __webpack_require__(2);
 var api = __webpack_require__(7);
 
 window.onload = function () {
@@ -421,8 +422,8 @@ var MyElement = function (_HTMLElement) {
 customElements.define('my-element', MyElement);
 
 module.exports = {
-    addTemplate: tacoData.add,
-    onUpdate: tacoData.onUpdate,
+    addTemplate: _tacodata.tacoData.add,
+    onUpdate: _tacodata.tacoData.onUpdate,
     go: api.go,
     next: api.next,
     previous: api.previous,
