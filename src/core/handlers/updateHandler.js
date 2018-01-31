48f7a7d93be782993888587156dcbe21463aad4f
dok <dumansky@gmail.com>
2018-01-31 15:22:03 +0200

    code review

--- src/core/handlers/updateHandler.js
+++ src/core/handlers/updateHandler.js
@@ -2,27 +2,27 @@ import {findKey, setByPath} from '../../utils/helpers.js';
 import {tacoData} from '../tacodata.js';
 
 function update(data){
-    var isUpdateCB = false;
+    var isDataChanged = false;
     for(let template in tacoData._main) {
         let key = findKey(data, template);
         for(let item in data[key]){
-            var controlKey = findKey(tacoData._main[key], item);
+            let controlKey = findKey(tacoData._main[key], item);
             if(controlKey) {
                 tacoData._main[key][item] = data[key][item];
-                isUpdateCB = true;
+                isDataChanged = true;
 
                 updateDom(controlKey, item, data[key][item]);
             }
         }
     }
-    if(isUpdateCB) {
+    if(isDataChanged) {
         tacoData.updateCB();
     }
 }
 
 
 function updateDom(template, control, value){
-    var dom = document.querySelector('[taco-template="' + template + '"] [taco-name="' + control.split(' ')[0] + '"]');
+    let dom = document.querySelector('[taco-template="' + template + '"] [taco-name="' + control.split(' ')[0] + '"]');
     if(dom){
         setByPath(dom, control.split(' ')[1], value);
     }
