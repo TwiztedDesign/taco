7066af7b16c55ed007d84258179bd1a5252ab684
Guy Brill <brill.guy1@gmail.com>
2018-01-31 11:38:40 +0200

    update handler tests for incorrect arguments - update function fix

--- src/core/handlers/updateHandler.js
+++ src/core/handlers/updateHandler.js
@@ -2,14 +2,20 @@ import {findKey} from '../../utils/helpers.js';
 import {tacoData} from '../tacodata.js';
 
 function update(data){
+    var isUpdateCB = false;
     for(let template in tacoData._main) {
         let key = findKey(data, template);
         for(let item in data[key]){
-            let mainKey = findKey(tacoData._main, key);
-            tacoData._main[mainKey][item] = data[key][item];
+            var controlKey = findKey(tacoData._main[key], item);
+            if(controlKey) {
+                tacoData._main[key][item] = data[key][item];
+                isUpdateCB = true;
+            }
         }
     }
-    tacoData.updateCB();
+    if(isUpdateCB) {
+        tacoData.updateCB();
+    }
 }
 
 module.exports = {
--- test/core/handlers/updateHandler.test.js
+++ test/core/handlers/updateHandler.test.js
@@ -3,18 +3,45 @@ const sinon          = require('sinon');
 const expect         = require('chai').expect;
 const updateHandler  = require('../../../src/core/handlers/updateHandler.js');
 
+/******************************* global spies ********************************/
+
+let updateCB = sinon.spy(tacoData, 'updateCB');
+
+/****************************************************************************/
+
 describe("handlers", () => {
     before(() => {
         tacoData.clear();
         tacoData.addTemplate('test', {visibility: true});
     });
 
+    describe("Update incorrect data", () => {
+        it("Should not update the template data and/or add the incorrect data or template", () => {
+            let initialData = tacoData._main;
+            // updating with non-existing control data
+            updateHandler.update({'test': {count: 2}});
+            expect(tacoData._main['test']['visibility']).to.equal(true);
+            expect(tacoData._main['test']['count']).to.equal(undefined);
+
+            // updating with empty data
+            updateHandler.update({'test': {}});
+            expect(tacoData._main).to.deep.equal(initialData);
+
+            // updating a non-existing template
+            updateHandler.update({'redBox': {count: 2}});
+            expect(tacoData._main['redBox']).to.equal(undefined);
+            expect(tacoData._main).to.deep.equal(initialData);
+
+            sinon.assert.notCalled(updateCB);
+        });
+    });
+
     describe("Update", () => {
         it("Should update the data in a given template as passed in the data obj", () => {
-            let updateCB = sinon.spy(tacoData, 'updateCB');
             updateHandler.update({'test': {visibility: false}});
             expect(tacoData._main['test']['visibility']).to.equal(false);
             sinon.assert.calledOnce(updateCB);
         });
     });
+
 });
