965ae9d415d0edd9793d4fcedc1b91bbf98f20ac
Guy Brill <brill.guy1@gmail.com>
2018-02-01 11:48:35 +0200

    helpers trim tests

--- test/utils/helpers.test.js
+++ test/utils/helpers.test.js
@@ -7,20 +7,34 @@ let obj = {
     }
 };
 
-describe('Helpers', function () {
-    describe('find key', function(){
-        it('Should find a key in a given object, case insensitive', function () {
+describe('Helpers', () => {
+    describe('find key', () => {
+        it('Should find a key in a given object, case insensitive', () => {
             expect(helpers.findKey(obj, 'Test')).to.equal('test');
         });
 
-        it('Should return undefined for a key in a given object if said key does not exists', function () {
+        it('Should return undefined for a key in a given object if said key does not exists', () => {
             expect(helpers.findKey(obj, 'testKey')).to.equal(undefined);
         });
 
-        it('Should return either key in a given object if two keys have the same value variation', function () {
+        it('Should return either key in a given object if two keys have the same value variation', () => {
             let obj = {test : {a : {}}, TesT : {b : {}}};
             expect(obj).to.have.own.property('TesT');
             expect(helpers.findKey(obj, 'TesT')).to.equal('test');
+        });
+    });
+
+    describe('Trim', () => {
+        it('Should trim the given character from the given string and return the string', () => {
+            expect(helpers.trim('**the quick brown fox jumps**', '*')).to.equal('the quick brown fox jumps');
+        });
+
+        it('Should not trim the given character if it does not exist in the given string and return the string', () => {
+            expect(helpers.trim('**the quick brown fox jumps**', ',')).to.equal('**the quick brown fox jumps**');
+        });
+
+        it('Should trim space if no character argument was passed and return the string', () => {
+            expect(helpers.trim(' the quick brown fox jumps ')).to.equal('the quick brown fox jumps');
         });
     });
 });
